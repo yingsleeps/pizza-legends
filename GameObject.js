998f38633475b9e3e99dfc39ae852486af87517e
ying <yingchou.0306@gmail.com>
2023-09-17 03:39:41 -0700

    refactored into game objects and sprites. sprites handle the display

---
+++ GameObject.js
@@ -0,0 +1,16 @@
+/* abstraction of the concept of people + items on screen */
+class GameObject {
+    // passing in an object
+    constructor(config) {
+        // pass in position of object or default
+        this.x = config.x || 0;
+        this.y = config.y || 0;
+
+        // pass in appearance 
+        this.sprite = new Sprite({
+            gameObject: this,
+            src: config.src || "/images/characters/people/hero.png",
+
+        });
+    }
+}
---
+++ Sprite.js
@@ -0,0 +1,53 @@
+class Sprite {
+    // pass in details for look of the sprite + animations 
+    constructor(config) {
+        /* animations are objects, keys will be name of the 
+        animation + each animation will have a series of frames */
+
+        // set up image
+        this.image = new Image();
+        this.image.src = config.src;
+        this.image.onload = () => {
+            this.isLoaded = true; // only draw after loaded
+
+        }
+
+        // shadow 
+        this.shadow = new Image();
+        this.useShadow = true; // config.useShadow || false
+        if (this.useShadow) {
+            this.shadow.src = "/images/characters/shadow.png";
+        }
+        this.shadow.onload = () => {
+            this.isShadowLoaded = true;
+        }
+
+        // configure animation + initial state
+        this.animations = config.animations || {
+            idleDown: [
+                [0,0]
+            ]
+        }
+        this.currentAnimation = config.currentAnimation || "idleDown";
+        this.currentAnimationFrame = 0;
+
+        // reference the game object
+        this.gameObject = config.gameObject;
+    }
+
+    // method to draw sprites
+    draw(ctx) {
+        // has adjustments built in
+        const x = this.gameObject.x * 16 - 8;
+        const y = this.gameObject.y * 16 - 18;
+
+        // wait til img is loaded to draw
+        this.isShadowLoaded && ctx.drawImage(this.shadow, x, y)
+        this.isLoaded && ctx.drawImage(this.image,
+            0, 0,
+            32, 32,
+            x, y,
+            32, 32
+        );
+    }
+}
