f25852f89f0144480a29123d07ce550dbcf8dfc0
ying <yingchou.0306@gmail.com>
2023-09-17 04:00:20 -0700

    implemented overworld maps, which have game objects + background imgs associated to them. also started the game loop, which will update the canvas every frame based on what is drawn

--- Overworld.js
+++ Overworld.js
@@ -7,41 +7,43 @@ class Overworld {
         this.element = config.element;
         this.canvas = this.element.querySelector(".game-canvas");
         this.ctx = this.canvas.getContext("2d");
+        this.map = null;
+    }
+
+    startGameLoop() {
+        /* not an infinite loop! fxn just fires every frame */
+        const step = () => {
+
+            // clean up the canvas before drawing
+            this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+            
+            // draw lower layer
+            this.map.drawLowerImage(this.ctx);
+
+            // draw upper layer
+            this.map.drawUpperImage(this.ctx);
+
+            // draw game objects 
+            Object.values(this.map.gameObjects).forEach(object =>{
+                object.x += 0.02;
+                object.sprite.draw(this.ctx);
+            })
+
+            // browser will call this fxn whenever a new frame begins
+            requestAnimationFrame(() => {
+                step();
+            })
+        }
+        step();
     }
 
     init() {
-        /* for html, to draw pixel data from an image onto a 
-        canvas context, u need to first load the img onto the 
-        browser in memory */
-        /* this img does not get injected into the DOM, we just
-        create it + after it gets downloaded, then we can copy 
-        the pixels onto the canvas */
+        this.map = new OverworldMap(window.OverworldMaps.Kitchen);
 
-        const image = new Image();
-        // call back that happens when the img loads
-        image.onload = () => {
-            // when img loads, draw it to the context
-            this.ctx.drawImage(image, 0, 0);
-        };
-        image.src = "/images/maps/DemoLower.png";
-
-        // place some game objects
-        const hero = new GameObject({
-            x: 5,
-            y: 6,
-        })
-
-        const npc1 = new GameObject({
-            x: 7,
-            y: 9,
-            src: "/images/characters/people/npc1.png",
-        })
+        // start game loop when browser starts
+        this.startGameLoop();
         
-        setTimeout(() => {
-             // draw the game objs
-            hero.sprite.draw(this.ctx);
-            npc1.sprite.draw(this.ctx);
-        }, 200)
+        
     }
 }
 
