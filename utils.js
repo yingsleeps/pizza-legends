ccce62bcb93cb40365f3fe6dcd8f0bdf4dfc8bd9
ying <yingchou.0306@gmail.com>
2023-10-02 00:23:03 -0700

    added a pacing util fxn used mainly for pacing the game

--- utils.js
+++ utils.js
@@ -36,5 +36,14 @@ const utils = {
     emitEvent(name, detail) {
         const event = new CustomEvent(name, { detail });
         document.dispatchEvent(event);
-    }
+    },
+
+    /* pacing function */
+    wait(ms) {
+        return new Promise(resolve => {
+            setTimeout(() => {
+                resolve()
+            }, ms)
+        })
+    },
 }
